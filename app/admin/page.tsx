6a174662ad8b307eba3aaa17657180717aa954ba
Nicoliiii <119983473+Nicoliiii@users.noreply.github.com>
2023-12-10 22:32:16 +0000

    fix: novoarrumadinho

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,9 +2,8 @@ export default function Admin(){
     return (
         <div>
         <h1 className="text-center text-xl">Área Administrativaa</h1>
-        <a href="/admin/Book">Listar Livros</a>
         <a href="/admin/Book/new">Cadastrar Livros</a>
-        
+        <a href="/admin/Book">Listar Livros</a>
 
     
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
-
 export default function Home() {
   return (
-    <div className="bg-[#3d3d3d]">
-      <div className="max-w-[1440px] m-auto ">
-        <h1 className="text-center text-xl">Aplicação de Livros</h1>
-        <a className="text-center text-white text-xl" href="/admin"> CLIQUE AQUI PARA ENTRAR NO SISTEMA</a>
-      </div>
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      height: '100vh', 
+      backgroundColor: '#3d3d3d' 
+    }}>
+      <h1 style={{ 
+        color: 'white', 
+        fontSize: '2rem', 
+        marginBottom: '1rem' 
+      }}>
+        Aplicação de Livros
+      </h1>
+      <a href="/admin" style={{ 
+        color: 'white', 
+        fontSize: '1.5rem', 
+        textDecoration: 'none', 
+        border: '1px solid white', 
+        padding: '0.5rem 1rem', 
+        borderRadius: '0.5rem' 
+      }}>
+        CLIQUE AQUI PARA ENTRAR NO SISTEMA
+      </a>
     </div>
   )
 }
