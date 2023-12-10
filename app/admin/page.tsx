b519e120c8597c5f3d47fa73bf0fb3906933c6d8
Nicoliiii <119983473+Nicoliiii@users.noreply.github.com>
2023-12-10 23:32:06 +0000

    fix: arrumando dnv

---
+++ app/admin/page.tsx
@@ -0,0 +1,48 @@
+export default function Admin(){
+    return (
+        <div style={{ 
+          display: 'flex', 
+          flexDirection: 'column', 
+          alignItems: 'center', 
+          justifyContent: 'flex-end', 
+          height: '100vh', 
+          backgroundColor: '#3d3d3d' 
+        }}>
+        <h1 style={{ 
+          color: 'white', 
+          fontSize: '2rem', 
+          marginBottom: '1rem' 
+        }}>
+          Área Administrativa
+        </h1>
+        <div style={{ 
+          display: 'flex', 
+          flexDirection: 'column', 
+          alignItems: 'center', 
+          marginBottom: '1rem' 
+        }}>
+          <a href="/admin/Book/new" style={{ 
+            color: 'white', 
+            fontSize: '1.5rem', 
+            textDecoration: 'none', 
+            border: '1px solid white', 
+            padding: '0.5rem 1rem', 
+            borderRadius: '0.5rem', 
+            marginBottom: '1rem' 
+          }}>
+            Cadastrar Livros
+          </a>
+          <a href="/admin/Book" style={{ 
+            color: 'white', 
+            fontSize: '1.5rem', 
+            textDecoration: 'none', 
+            border: '1px solid white', 
+            padding: '0.5rem 1rem', 
+            borderRadius: '0.5rem' 
+          }}>
+            Listar Livros
+          </a>
+        </div>
+        </div>
+    )
+}
