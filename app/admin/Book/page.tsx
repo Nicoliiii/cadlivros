7e25fd4b5f72aaeb7e5528b22c01cac40adba653
Nicoliiii <119983473+Nicoliiii@users.noreply.github.com>
2023-12-10 23:44:38 +0000

    fix: branquinho

--- app/admin/Book/page.tsx
+++ app/admin/Book/page.tsx
@@ -12,33 +12,61 @@ export default async function ListBook() {
     }
     const { rows } = await sql`SELECT * from book`;
     return (
-        <div>
-            <h1 className="text-center text-white">Lista de Livros</h1>
-
-            <table>
+        <div style={{ 
+          display: 'flex', 
+          flexDirection: 'column', 
+          alignItems: 'center', 
+          justifyContent: 'flex-end', 
+          height: '100vh', 
+          backgroundColor: 'white' 
+        }}>
+            <h1 style={{ 
+              color: 'black', 
+              fontSize: '2rem', 
+              marginBottom: '1rem' 
+            }}>
+              Lista de Livros
+            </h1>
+            <table style={{ 
+              color: 'black', 
+              borderCollapse: 'collapse' 
+            }}>
                 <thead>
-                    <tr> <td>Título </td> <td>Autor</td> <td>Número de Páginas</td></tr>
+                    <tr> 
+                      <td style={{ border: '1px solid black', padding: '0.5rem' }}>Título</td> 
+                      <td style={{ border: '1px solid black', padding: '0.5rem' }}>Autor</td> 
+                      <td style={{ border: '1px solid black', padding: '0.5rem' }}>Número de Páginas</td>
+                    </tr>
                 </thead>
                 <tbody>
                     {
                         rows.map((book) => {
                             return (
-                                <tr key={book.id}><td>{book.titulo}</td> <td>{book.autor}</td> <td>{book.num_paginas}</td>
-                                <td>
-                                    <form >
-                                     <input type="text" hidden name="id" value={book.id}/>   
-                                    <button className="text-red-700" formAction={deleteBook}>Excluir</button>
+                                <tr key={book.id}>
+                                  <td style={{ border: '1px solid black', padding: '0.5rem' }}>{book.titulo}</td> 
+                                  <td style={{ border: '1px solid black', padding: '0.5rem' }}>{book.autor}</td> 
+                                  <td style={{ border: '1px solid black', padding: '0.5rem' }}>{book.num_paginas}</td>
+                                  <td>
+                                    <form>
+                                      <input type="text" hidden name="id" value={book.id}/>   
+                                      <button formAction={deleteBook} style={{ 
+                                        backgroundColor: '#FF0000', 
+                                        color: 'white', 
+                                        padding: '0.5rem 1rem', 
+                                        borderRadius: '0.5rem', 
+                                        border: 'none', 
+                                        cursor: 'pointer' 
+                                      }}>
+                                        Excluir
+                                      </button>
                                     </form>
-                                
-                                </td> 
+                                  </td> 
                                 </tr>
                             )
                         })
                     }
                 </tbody>
             </table>
-
-
         </div>
     )
 }
