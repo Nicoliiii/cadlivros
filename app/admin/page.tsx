9539470af00fb814b20207fcfac4e1d0b15ea795
Nicoliiii <119983473+Nicoliiii@users.noreply.github.com>
2023-12-10 22:20:16 +0000

    fix: teste0101

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 export default function Admin(){
     return (
         <div>
-        <h1 className="text-center text-xl">Área Administrativa</h1>
+        <h1 className="text-center text-xl">Área Administrativaa</h1>
         <a href="/admin/Book">Listar Livros</a>
         <a href="/admin/Book/new">Cadastrar Livros</a>
         
