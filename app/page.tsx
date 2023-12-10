6e9fbaf2eb98de657f1db42b9da91838090cef69
Nicoliiii <119983473+Nicoliiii@users.noreply.github.com>
2023-12-10 22:24:04 +0000

    fix: arrumadinho

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-   <h1>bobo</h1>
+    <div className="bg-[#3d3d3d]">
+      <div className="max-w-[1440px] m-auto ">
+        <h1 className="text-center text-xl">Aplicação de Livros</h1>
+        <a className="text-center text-white text-xl" href="/admin"> CLIQUE AQUI PARA ENTRAR NO SISTEMA</a>
+      </div>
+    </div>
   )
 }
