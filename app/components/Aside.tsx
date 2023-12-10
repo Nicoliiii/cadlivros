a129e0f888d47520fdc0bf1749495a206fa1fbbd
Nicoliiii <119983473+Nicoliiii@users.noreply.github.com>
2023-12-10 23:39:22 +0000

    fix: meudeussss

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -1,8 +1,6 @@
 export default function Aside() {
     return (
         <div className="w-1/6">
-            <a href="/admin/Book">Listar Livros</a> <br></br>
-            <a href="/admin/Book/new">Cadastrar Livros </a>
 
         </div>
     )
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,6 @@
 export default function NavBar(){
     return(
         <div className="p-12 flex justify-between items-center">
-           
-            <h1>Navegação</h1>
-            <h1>Nome Usuario</h1>
         </div>
 
     )
