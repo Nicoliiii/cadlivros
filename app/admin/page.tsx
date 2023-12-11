18bcdc9311420d2e44d66ef6f960b24e2f42e2d3
Nicoliiii <119983473+Nicoliiii@users.noreply.github.com>
2023-12-11 09:36:15 +0000

    fix: deuerrinho

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,65 +1,48 @@
-import React, { useState } from 'react';
-
 export default function Admin() {
-    const [hoverNew, setHoverNew] = useState(false);
-    const [hoverList, setHoverList] = useState(false);
-
-    return (
-        <div style={{ 
-          display: 'flex', 
-          flexDirection: 'column', 
-          alignItems: 'center', 
-          justifyContent: 'center', 
-          height: '100vh', 
-          backgroundColor: 'white' 
-        }}>
-        <h1 style={{ 
-          color: 'black', 
-          fontSize: '2rem', 
+  return (
+      <div style={{ 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        height: '100vh', 
+        backgroundColor: 'white' 
+      }}>
+      <h1 style={{ 
+        color: 'black', 
+        fontSize: '2rem', 
+        marginBottom: '1rem' 
+      }}>
+        Área Administrativa
+      </h1>
+      <div style={{ 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center', 
+        marginBottom: '1rem' 
+      }}>
+        <a href="/admin/Book/new" style={{ 
+          color: 'white', 
+          fontSize: '1.5rem', 
+          textDecoration: 'none', 
+          border: '1px solid white', 
+          padding: '0.5rem 1rem', 
+          borderRadius: '0.5rem', 
           marginBottom: '1rem' 
         }}>
-          Área Administrativa
-        </h1>
-        <div style={{ 
-          display: 'flex', 
-          flexDirection: 'column', 
-          alignItems: 'center', 
-          gap: '1rem' 
+          Cadastrar Livros
+        </a>
+        <a href="/admin/Book" style={{ 
+          color: 'white', 
+          fontSize: '1.5rem', 
+          textDecoration: 'none', 
+          border: '1px solid white', 
+          padding: '0.5rem 1rem', 
+          borderRadius: '0.5rem' 
         }}>
-          <a 
-            href="/admin/Book/new" 
-            style={{ 
-              color: hoverNew ? '#32CD32' : 'black', 
-              fontSize: '1.5rem', 
-              textDecoration: 'none', 
-              border: '1px solid black', 
-              padding: '0.5rem 1rem', 
-              borderRadius: '0.5rem', 
-              marginBottom: '1rem',
-              transition: 'color 0.3s ease',
-            }}
-            onMouseEnter={() => setHoverNew(true)}
-            onMouseLeave={() => setHoverNew(false)}
-          >
-            Cadastrar Livros
-          </a>
-          <a 
-            href="/admin/Book" 
-            style={{ 
-              color: hoverList ? '#32CD32' : 'black', 
-              fontSize: '1.5rem', 
-              textDecoration: 'none', 
-              border: '1px solid black', 
-              padding: '0.5rem 1rem', 
-              borderRadius: '0.5rem',
-              transition: 'color 0.3s ease',
-            }}
-            onMouseEnter={() => setHoverList(true)}
-            onMouseLeave={() => setHoverList(false)}
-          >
-            Listar Livros
-          </a>
-        </div>
-        </div>
-    )
+          Listar Livros
+        </a>
+      </div>
+      </div>
+  )
 }
