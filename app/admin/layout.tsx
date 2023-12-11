67e8aad3e3ac87b02d3b424544e55f02ccfa2bfe
Nicoliiii <119983473+Nicoliiii@users.noreply.github.com>
2023-12-11 00:33:46 +0000

    fix: tomara que seja isso

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,48 +1,25 @@
-export default function Admin() {
+import Aside from "../components/Aside"
+import NavBar from "../components/NavBar"
+
+export default function AdminLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
     return (
-        <div style={{ 
-          display: 'flex', 
-          flexDirection: 'column', 
-          alignItems: 'center', 
-          justifyContent: 'center', 
-          height: '100vh', 
-          backgroundColor: 'white' 
-        }}>
-        <h1 style={{ 
-          color: 'black', 
-          fontSize: '2rem', 
-          marginBottom: '1rem' 
-        }}>
-          Área Administrativa
-        </h1>
-        <div style={{ 
-          display: 'flex', 
-          flexDirection: 'column', 
-          alignItems: 'center', 
-          marginBottom: '1rem' 
-        }}>
-          <a href="/admin/Book/new" style={{ 
-            color: 'black', 
-            fontSize: '1.5rem', 
-            textDecoration: 'none', 
-            border: '1px solid black', 
-            padding: '0.5rem 1rem', 
-            borderRadius: '0.5rem', 
-            marginBottom: '1rem' 
-          }}>
-            Cadastrar Livros
-          </a>
-          <a href="/admin/Book" style={{ 
-            color: 'black', 
-            fontSize: '1.5rem', 
-            textDecoration: 'none', 
-            border: '1px solid black', 
-            padding: '0.5rem 1rem', 
-            borderRadius: '0.5rem' 
-          }}>
-            Listar Livros
-          </a>
+        <div className="max-w-[1024px] m-auto">
+            <NavBar />
+            <div className="flex">
+                <Aside />
+                <div className="p-12 w-screen">
+                {children}
+                </div>
+                
+
+            </div>
+
         </div>
-        </div>
+
+
     )
 }
